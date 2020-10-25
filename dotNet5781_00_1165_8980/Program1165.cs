7180f72064ae5f64b60443771f200f26b03ea567
hadasash <hadasasharon@gmail.com>
2020-10-25 18:31:58 +0200

    First Part of Targil 00

---
+++ dotNet5781_00_1165_8980/Program1165.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_1165_8980
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1165();
+            Welcome8980();
+            Console.ReadKey();
+        }
+
+        private static void Welcome1165()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome8980();
+        }
+            
+
+
+    }
+}
