b6bfef4a50aed0dd9feaa36033371c32af67bd95
adashken <dindan8938@gmail.com>
2020-10-25 18:47:47 +0200

    Adi's changes

---
+++ dotNet5781_00_1165_8980/Program8980.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_1165_8980
+{
+    partial class Program
+    {
+     static partial void Welcome8980()
+      {
+            Console.WriteLine("I'm also here");
+
+      }
+            
+
+    }
+}
