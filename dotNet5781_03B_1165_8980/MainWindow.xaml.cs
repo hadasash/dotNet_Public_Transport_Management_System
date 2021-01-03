e1bfcde380ed9fa8266d55da4e944cb957e43dd8
hadasash <hadasasharon@gmail.com>
2021-01-03 09:48:09 +0200

    ש

--- dotNet5781_03B_1165_8980/MainWindow.xaml.cs
+++ dotNet5781_03B_1165_8980/MainWindow.xaml.cs
@@ -213,6 +213,7 @@ namespace dotNet5781_03B_1165_8980
 
         private void bcAdd_Click(object sender, RoutedEventArgs e)
         {
+            this.Cursor
             Bus MyNewBus = new Bus();
             MyNewBus.BeginingOfWork = new DateTime(2000, 1, 1);
             MyNewBus.LastTratment = new DateTime(2000, 1, 1);
