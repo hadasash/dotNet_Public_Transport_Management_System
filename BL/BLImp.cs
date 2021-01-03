84bb1fc737642333827575e05c3385bdf9ff220d
adashken <dindan8938@gmail.com>
2021-01-03 09:51:59 +0200

    something

---
+++ BL/BLImp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DLAPI;
+using BLAPI;
+using System.Threading;
+using BO;
+//using BO;
+
+namespace BL
+{
+    class BLImp : IBL //internal
+    {
+        IDL dl = DLFactory.GetDL();
+
+    }
+}
