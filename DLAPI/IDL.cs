d26a661744970ad473b1239142e92c692f947be4
hadasash <hadasasharon@gmail.com>
2021-01-09 20:53:34 +0200

    Add DL functions

--- DLAPI/IDL.cs
+++ DLAPI/IDL.cs
@@ -15,6 +15,10 @@ namespace DLAPI
         #region Station
         DO.Station GetStation(int code);
         IEnumerable<DO.Station> GetAllStations();
+        void AddStation(DO.Station person);
+        void UpdateStation(DO.Station person);
+        void UpdateStation(int code, Action<DO.Station> update); //method that knows to updt specific fields in Station
+        void DeleteStation(int code);
         #endregion
 
         #region Line
@@ -23,7 +27,11 @@ namespace DLAPI
         #endregion
 
         #region Station In Line
-        IEnumerable<DO.LineStation> GetStationsLineInList(Predicate<DO.LineStation> predicate);
+        IEnumerable<DO.LineStation> GetLinesInStation(Predicate<DO.LineStation> predicate);
+        void AddLineInStation(int lineID, int statCode, float grade = 0);
+        void UpdateLineInStation(int lineID, int statCode, float lineStationIndex);
+        void DeleteLineInStation(int lineID, int statCode);
+        void DeleteLineInStation(int lineID);
         #endregion
     }
 
--- DLObject/DLObject.cs
+++ DLObject/DLObject.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using DLAPI;
+using DO;
 //using DO;
 using DS;
 
@@ -43,10 +44,47 @@ namespace DL
             return from station in DataSource.listStations
                    select station.Clone();
         }
+
+        public void AddStation(DO.Station station)
+        {
+            if (DataSource.listStations.FirstOrDefault(s => s.Code == station.Code) != null)
+                throw new DO.BadPersonIdException(person.ID, "Duplicate person ID"); //TO DO 
+            DataSource.listStations.Add(station.Clone());
+        }
+
+        public void DeleteStation(int code)
+        {
+            DO.Station stat = DataSource.listStations.Find(s => s.Code == code);
+
+            if (stat != null)
+            {
+                DataSource.listStations.Remove(stat);
+            }
+            else
+                throw new DO.BadPersonIdException(id, $"bad person id: {id}"); //TO DO
+        }
+
+        public void UpdatePerson(DO.Station station)
+        {
+            DO.Station stat = DataSource.listStations.Find(p => p.Code == station.Code);
+
+            if (stat != null)
+            {
+                DataSource.listStations.Remove(stat);
+                DataSource.listStations.Add(station.Clone());
+            }
+            else
+                throw new DO.BadPersonIdException(station.ID, $"bad person id: {station.ID}"); //TO DO 
+        }
+
+        public void UpdatePerson(int id, Action<DO.Person> update)
+        {
+            throw new NotImplementedException();
+        }
         #endregion
 
         #region Line
-        
+
         public DO.Line GetLine(int lineId)
         {
             return DataSource.listLines.Find(c => c.LineId == lineId).Clone();
@@ -59,22 +97,47 @@ namespace DL
         }
         public IEnumerable<DO.LineStation> GetStationsLineInList(Predicate<DO.LineStation> predicate)
         {
-            //option A - not good!!! 
-            //produces final list instead of deferred query and does not allow proper cloning:
-            // return DataSource.listStudInCourses.FindAll(predicate);
-
-            // option B - ok!!
-            //Returns deferred query + clone:
-            //return DataSource.listStudInCourses.Where(sic => predicate(sic)).Select(sic => sic.Clone());
-
-            // option c - ok!!
-            //Returns deferred query + clone:
             return from sil in DataSource.listLineStation
                    where predicate(sil)
                    select sil.Clone();
         }
 
-        #endregion 
+        public void UpdateStation(Station person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStation(int code, Action<Station> update)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LineStation> GetLinesInStation(Predicate<LineStation> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddLineInStation(int lineID, int statCode, float grade = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateLineInStation(int lineID, int statCode, float lineStationIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteLineInStation(int lineID, int statCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteLineInStation(int lineID)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
 
     }
 }
