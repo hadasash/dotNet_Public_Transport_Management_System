2bd14d31fd21b715f1480865c7dafb4a218afaa5
hadasash <hadasasharon@gmail.com>
2021-01-10 10:10:59 +0200

    hadasa

--- BL/BLImp.cs
+++ BL/BLImp.cs
@@ -13,69 +13,116 @@ namespace BL
     {
         IDL dl = DLFactory.GetDL();
         #region station
-        //Hadasa: finish station
-        public BO.Station GetStation(int code)
-        {
-            DO.Station stationDO; 
-            stationDO = dl.GetStation(code);
-            
-            return stationDoBoAdapter(stationDO);
-        }
+        //Hadasa: finish station student =person=station. course =line
         BO.Station stationDoBoAdapter(DO.Station stationDO)
         {
             BO.Station stationBO = new BO.Station();
-            DO.Station newstationDO;
-            int code = stationDO.Code;
-            //try
-            //{
-            //    newstationDO = dl.GetStation(code);
-            //}
-            //catch (DO.BadPersonIdException ex)
-            //{
-            //    throw new BO.BadStudentIdException("Student ID is illegal", ex);
-            //}
-           
-
+            DO.Station personDO;
+            int id = stationDO.Code;
+            try
+            {
+                personDO = dl.GetStation(id);
+            }
+            catch (DO.BadStationCodeException ex)
+            {
+                throw new BO.BadStationCodeException("Station code is illegal", ex);
+            }
+            personDO.CopyPropertiesTo(stationBO);
+            
             stationDO.CopyPropertiesTo(stationBO);
-
-            stationBO.ListOfLines = from sil in dl.GetLinesInStation(sil => sil.Station == code)
-                                    let line = dl.GetLine(sil.LineId)
-                                       select line.CopyToListOfLines(sil);
-      
+           
+            stationBO.Longitude = from sil in dl.GetStationInLinesList(sil => sil.PersonId == code)
+                                      let line = dl.GetLine(sil.LINEId)
+                                      select line.CopyToStudentCourse(sil);
+           
             return stationBO;
         }
-        public IEnumerable<BO.Station> GetAllStations()
+
+        public BO.Station GetStation(int id)
         {
-            
-            return from item in dl.GetAllStations()
-                   select stationDoBoAdapter(item);
+            DO.Station stationDO;
+            try
+            {
+                stationDO = dl.GetStation(id);
+            }
+            catch (DO.BadStationCodeException ex)
+            {
+                throw new BO.BadStationCodeException("station code does not exist ", ex);
+            }
+            return stationDoBoAdapter(stationDO);
         }
+
+        public IEnumerable<BO.Station> GetAllStatons()
+        {
+         
+            return from stationDO in dl.GetAllStations()
+                   orderby stationDO.Code
+                   select stationDoBoAdapter(stationDO);
+        }
+        public IEnumerable<BO.Station> GetStationsBy(Predicate<BO.Station> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BO.ListedPerson> GetStationIDNameList()
+        {
+            return from item in dl.GetStationListWithSelectedFields((StationDO) =>
+            {
+                try { Thread.Sleep(1500); } catch (ThreadInterruptedException e) { }
+                return new BO.ListedPerson() { ID = stationDO.ID, Name = dl.GetPerson(stationDO.ID).Name };
+            })
+                   let stationBO = item as BO.ListedPerson
+                   select stationBO;
+        }
+
+        public void UpdateStudentPersonalDetails(BO.Station station)
+        {          
+            DO.Station personDO = new DO.Station();
+            station.CopyPropertiesTo(personDO);
+            try
+            {
+                dl.UpdateStation(personDO);
+            }
+            catch (DO.BadStationCodeException ex)
+            {
+                throw new BO.BadStationCodeException("Student ID is illegal", ex);
+            }
+
+            //Update DO.Student            
+            DO.Station stationDO = new DO.Station();
+            station.CopyPropertiesTo(stationDO);
+            try
+            {
+                dl.UpdateStation(stationDO);
+            }
+            catch (DO.BadStationCodeException ex)
+            {
+                throw new BO.BadStationCodeException("Student ID is illegal", ex);
+            }
+
+        }
+        public void DeleteStation(int id)
+        {
+            try
+            {
+                dl.DeleteStation(id);
+                dl.DeleteStation(id);
+                dl.DeleteStationFromAllLines(id);
+            }
+            catch (DO.BadStationCodeLineID ex)
+            {
+                throw new BO.BadStationCodeLineID("Station ID and line ID is Not exist", ex);
+            }
+            catch (DO.BadStationCodeException ex)
+            {
+                throw new BO.BadStationCodeException("Person id does not exist or he is not a station", ex);
+            }
+        }
+
         #endregion
         #region Line
         //Adi- Add, Update, Delete
-        BO.Course courseDoBoAdapter(DO.Course courseDO)
-        {
-            BO.Course courseBO = new BO.Course();
-            int id = courseDO.ID;
-            courseDO.CopyPropertiesTo(courseBO);
-
-            courseBO.Lecturers = from lic in dl.GetLecturersInCourseList(lic => lic.CourseId == id)
-                                 let course = dl.GetCourse(lic.CourseId)
-                                 select (BO.CourseLecturer)course.CopyPropertiesToNew(typeof(BO.CourseLecturer));
-            return courseBO;
-        }
-        public IEnumerable<BO.Course> GetAllCourses()
-        {
-            return from crsDO in dl.GetAllCourses()
-                   select courseDoBoAdapter(crsDO);
-        }
-
-        public IEnumerable<BO.StudentCourse> GetAllCoursesPerStudent(int id)
-        {
-            return from sic in dl.GetStudentsInCourseList(sic => sic.PersonId == id)
-                   let course = dl.GetCourse(sic.CourseId)
-                   select course.CopyToStudentCourse(sic);
-        }
+      
         #endregion
     }
 
